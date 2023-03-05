19d172a39fe3a6f526ad33ad22b0838c4b7a601c
jonhteper <jonhteper@protonmail.com>
2023-03-04 19:32:05 -0600

    feat: restructure Rule type

--- src/model/rule.rs
+++ src/model/rule.rs
@@ -1,21 +1,32 @@
-use serde_json::map::Map;
-use serde_json::value::Value as Value;
-use crate::model::permission::Permission;
-use crate::model::assertion::Assertion;
 use crate::errors::MinosError;
+use crate::model::assertion::{Assertion, ToAssertions};
+use crate::model::attribute::Attribute;
+use crate::model::permission::{Permission, ToPermissions};
+use derive_builder::Builder;
+use serde_json::map::Map;
+use serde_json::value::Value;
 
-pub trait ToRule {
-    fn to_rule(&self, object: &Map<String, Value>) -> Result<Rule, MinosError>;
+pub trait ToRule: ToPermissions + ToAssertions {
+    fn to_rule(&self) -> Result<Rule, MinosError>;
 }
 
-pub struct Rule {
+#[derive(Debug, Default, Clone)]
+pub struct ActorRuleAttributes<'at> {
+    groups: Vec<String>,
+    attributes: Vec<Attribute<'at>>,
+}
+
+#[derive(Debug, Default, Clone)]
+pub struct ResourceRuleAttributes<'at> {
+    owner_check: bool,
+    attributes: Vec<Attribute<'at>>,
+}
+
+#[derive(Default, Clone, Debug, Builder)]
+pub struct Rule<'a> {
     permissions: Vec<Permission>,
-    actor_attributes: Map<String, Value>,
-    resource_attributes: Map<String, Value>,
-    environment_attributes: Map<String, Value>,
+    actor_attributes: ActorRuleAttributes<'a>,
+    resource_attributes: ResourceRuleAttributes<'a>,
+    environment_attributes: Vec<Attribute<'a>>,
     assertions: Vec<Assertion>,
-}
-
-impl Rule {
-
 }
