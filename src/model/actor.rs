8c8193092e716dc4301a01d99260e5e06e5d08aa
jonhteper <jonhteper@protonmail.com>
2023-03-05 02:26:48 -0600

    feat: use json map for Actor::attributes

--- src/model/actor.rs
+++ src/model/actor.rs
@@ -1,10 +1,11 @@
+use serde_json::{Map, Value};
 use crate::errors::MinosError;
 use crate::model::attribute::Attribute;
 
 pub struct Actor<'a> {
     id: &'a str,
     groups: Vec<String>,
-    attributes: Vec<Attribute<'a>>,
+    attributes: Map<String, Value>,
 }
 
 pub trait ToActor {
